2b533fa4a55812e20929c4cbdd761a25eebc0a66
Jack Caltagirone <35774493+JackCaltagirone@users.noreply.github.com>
2021-01-28 16:24:29 +0000

    Create AuthorController.java

---
+++ src/main/java/guru/springframework/spring5webapp/controllers/AuthorController.java
@@ -0,0 +1,30 @@
+package guru.springframework.spring5webapp.controllers;
+
+import guru.springframework.spring5webapp.Repositories.AuthorRepo;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AuthorController {
+	
+	//instance of AuthorRepo. injects authorRepo
+	private final AuthorRepo authorRepo;
+    //controller so its  Spring managed controller
+	public AuthorController(AuthorRepo authorRepo) {
+		this.authorRepo = authorRepo;
+	}
+
+    //map to url - /authors
+	@RequestMapping("/authors")
+	public String getAuthors(Model model) {
+		
+		//looks at book repo for list of authors
+        model.addAttribute("author", authorRepo.findAll());
+		
+		return "Auther/list";
+		
+	}
+	
+	
+}
